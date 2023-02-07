1d87ab1ee216c5bf8d415c43aa8d8a05df13f5b1
Vusal Ismayilov <woosal@pm.me>
2023-02-07 23:23:12 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -33,15 +33,15 @@ def sidebar():
                 current_sidebar_values.insert(0, "")
 
             if "_" in i:
-                j = i.replace("_", " ")
+                j = i.replace("_", " ").capitalize()
             else:
-                j = i
+                j = i.capitalize()
 
             current_user_input[i] = st.selectbox(
                 j, current_sidebar_values, index=current_sidebar_values.index("")
             )
 
-        submitted = st.form_submit_button("Submit")
+        submitted = st.form_submit_button("Ara")
         if submitted:
             # Remove the "" values from the user input
             for z in current_user_input:
@@ -61,30 +61,52 @@ def draw_map(coordinates_data: dict):
     if coordinates_data:
         m = folium.Map(location=[37.7981263, 36.1829598], zoom_start=6)
 
-        for x in coordinates_data:
-            try:
-                if (
-                        x["lat"]
-                        and x["lon"]
-                        and x["lat"] != ""
-                        and x["lon"] != ""
-                ):
-                    folium.CircleMarker(
-                        tooltip=f"{x['konum_ilce']}",
-                        location=[x["lat"], x["lon"]],
-                        popup=f"Kişi Sayısı: {x['kisi_sayisi']}"
-                              f" Kat: {x['kat']}",
-                        radius=10,
-                        fill=True,
-                        fill_color="red",
-                        color="red",
-                        fill_opacity=0.5,
-                    ).add_to(m)
+        with st.expander("Show Data"):
+            for x in coordinates_data:
+                try:
+                    if (
+                            x["lat"]
+                            and x["lon"]
+                            and x["lat"] != ""
+                            and x["lon"] != ""
+                    ):
+                        folium.CircleMarker(
+                            tooltip=f"{x['konum_ilce']}",
+                            location=[x["lat"], x["lon"]],
+                            popup=f"Kişi Sayısı: {x['kisi_sayisi']}"
+                                  f" Kat: {x['kat']}",
+                            radius=10,
+                            fill=True,
+                            fill_color="red",
+                            color="red",
+                            fill_opacity=0.5,
+                        ).add_to(m)
 
-            except KeyError:
-                pass
+                    if x['lat'] and x['lon'] and x['lat'] != '' and x['lon'] != '':
+                        location = f"http://maps.google.com/?ll={x['lat']},{x['lon']}"
+                    else:
+                        location = "Konum Bilgisi Yok"
+
+                    st.info(f"""
+                    İl: {x['konum_il']} |
+                    İlçe: {x['konum_ilce']} |
+                    Mahalle: {x['konum_mahalle']} \n
+                    İsim Soyisim: {x['isimsoyisim']} |
+                    Kişi Sayısı: {x['kisi_sayisi']} |
+                    Telefon Numrası: {x['telefon']} \n
+                    Adres: {x['adres']} |
+                    Apartman: {x['apartman']} |
+                    Sokak: {x['sokak']} |
+                    Blok: {x['blok_no']} |
+                    Kat: {x['kat']} \n
+                    Google Maps Linki: {location}
+                    """)
+
+                except KeyError:
+                    pass
 
         st_folium.folium_static(m, width=1400, height=600)
+
 
 
 st.set_page_config(layout="wide")
